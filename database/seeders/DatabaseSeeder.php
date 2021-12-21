ae916c5a92c9597dca3030e1f828fc8f309a8601
Syafiza <syafiza.senin@pipeline-network.com>
2021-12-21 13:55:28 +0800

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'user_name' => 'Abc',
+            'user_name' => 'Syafiza Senin',
             'email' => 'abc@gmail.com',
             'user_identity_no'=>'9999',
             'jenis'=>'pekerja',
@@ -68,12 +68,32 @@ class DatabaseSeeder extends Seeder
 
         DB::table('fiza_item_infos')->insert([
             'katalog_id'=>'1',
-            'pembekal_id'=>'2',
+            'pembekal_id'=>'1',
             'item_name'=>'HP Printer DeskJet Ink Advantage 2336',
             'item_price'=>'450.60',
             'item_unit'=>'n/a',
             'start_date'=>'2021-01-01',
             'end_date'=>'2021-10-10'
+        ]);
+
+        DB::table('fiza_faqs')->insert([
+            'faq_section'=>'Pembekal',
+            'faq_question'=>'Adakah perolehan kerja boleh dilaksanakan dengan mengguna pakai Sistem ePerolehan Baharu?',
+            'faq_answer'=>'Tidak. Sistem ePerolehan Baharu hanya memberi perkhidmatan perolehan untuk bekalan dan perkhidmatan sahaja',
+            'faq_status'=>'aktif',
+            'user_id'=>'1',
+            'faq_created_by'=>'syafiza senin',
+            'faq_updated_by'=>'syafiza senin'
+        ]);
+
+        DB::table('fiza_faqs')->insert([
+            'faq_section'=>'Pembekal',
+            'faq_question'=>'Bagaimana jika PTJ tidak memenuhi infrastruktur minima yang telah ditetapkan? Adakah pihak Kementerian masing-masing, Kementerian Kewangan atau pihak Commerce Dot Com Sdn Bhd akan menyediakan kemudahan komputer kepada pihak PTJ?',
+            'faq_answer'=>'Aktiviti pengumpulan maklumat komputer telah dijalankan oleh pihak Commerce Dot Com Sdn Bhd bermula 3 Mac 2014. Maklumat tersebut telah diserahkan kepada pihak Jabatan Akauntan Negara Malaysia (JANM) AG melalui Seksyen ePerolehan pada 18 April 2014 untuk tindakan selanjutnya. Walau bagaimanapun, PTJ masih boleh mengakses sistem ePerolehan Baharu tetapi terdapat kemungkinan menghadapi beberapa masalah seperti akses rangkaian yang perlahan dari luar bandar. Talian (1Govnet) yang tidak ditambah atau kurang dari keperluan minima juga akan memberi impak kepada kelancaran pengunaan sistem ePerolehan Baharu.',
+            'faq_status'=>'aktif',
+            'user_id'=>'1',
+            'faq_created_by'=>'syafiza senin',
+            'faq_updated_by'=>'syafiza senin'
         ]);
 
     }
